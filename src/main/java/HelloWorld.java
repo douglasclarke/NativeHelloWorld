b7608bc8569b1e717e8ff72e971b4a99da8ebb82
Doug Clarke <douglas.clarke@oracle.com>
2023-03-12 23:42:17 -0400

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,14 @@
+import org.yaml.snakeyaml.Yaml;
+
 public class HelloWorld { 
+    
     public static void main(String[] args) { 
-        System.out.println("Hello, World!"); } }
+        System.out.println("Hello, World!"); 
+
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("Hello", "World");
+
+       Yaml yaml = new Yaml();
+       yaml.dump(data, System.out);
+    } 
+}
