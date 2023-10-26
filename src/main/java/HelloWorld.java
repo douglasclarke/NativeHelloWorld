69b02e705a371fa23b2da3821e80aaa6834b83a7
Doug Clarke <douglas.clarke@oracle.com>
2023-10-26 10:12:14 -0400

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,11 @@ import java.util.Map;
 import java.util.HashMap;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Optional;
 
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.yaml.snakeyaml.Yaml;
 
 public class HelloWorld { 
@@ -11,8 +15,13 @@ public class HelloWorld {
         Map<String, Object> data = new HashMap<String, Object>();
         data.put("Hello", "World");
 
-       Yaml yaml = new Yaml();
-       BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-       yaml.dump(data, out);
+        Yaml yaml = new Yaml();
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        yaml.dump(data, out);
+
+        TypeDefinitionRegistry typeDefinitionRegistry = new SchemaParser().parse(schema);
+        Optional<?> typeDef = typeDefinitionRegistry.getType("Query");
+
+        System.out.println(typeDef.toString());
     } 
 }
